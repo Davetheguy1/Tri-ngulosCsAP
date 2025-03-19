20a8a5d8a6fd452f3011c2e173a17c0f21759df3
danilo <danilocencigg@gmail.com>
2025-03-18 23:22:07 -0300

    Add project files.

---
+++ TriângulosCsAP/Methods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriângulosCsAP
+{
+    class Methods
+    {
+      public static void InputRegister()
+        {
+            Console.Clear();
+            Console.WriteLine("Declare a Medida do Primeiro Lado:\n");
+            double X = double.Parse(Console.ReadLine());
+            Console.WriteLine("Declare a Medida do Segundo Lado:\n");
+            double Y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Declare a Medida do Terceiro Lado:\n");
+            double Z = double.Parse(Console.ReadLine());
+
+            ValidateTriangle(X, Y, Z);
+            CalcPerimeter(X, Y, Z);
+            ClassTriangle(X, Y, Z);
+            Program.Enquire();
+        }
+
+        public static void ValidateTriangle(double x, double y, double z)
+        {
+            
+            if (x <= 0 || y <= 0 || z <= 0)
+            {
+                Console.WriteLine("Medidas Inválidas, todos os valores devem ser diferentes ou maiores do que zero\n");
+                InputRegister();
+            } else if (x + y < z || x +z < y || y +z < x){
+                Console.WriteLine("Medidas Inválidas, a soma de quaisquer dois lados deve ser maior que o terceiro lado\n");
+                InputRegister();
+            } else
+            {
+                Console.WriteLine("\nO Triângulo é Valido");
+            }
+        }
+
+        public static void CalcPerimeter(double x, double y, double z)
+        {
+            double result = (x + y + z);
+            double roundedResult = Math.Round(result, 2);
+            Console.WriteLine($"Perímetro : {roundedResult}");
+        }
+
+        public static void ClassTriangle(double x, double y, double z)
+        {
+            if (x == y && x == z && y == z)
+            {
+                Console.WriteLine("Tipo de Triângulo : Equilátero\n");
+            } else if (x != y && x != z && y != z)
+            {
+                Console.WriteLine("Tipo de Triângulo : Escaleno\n");
+            } else
+            {
+                Console.WriteLine("Tipo de Triângulo : Isósceles\n");
+            }
+        }
+        
+     
+            
+
+            
+        
+
+    
+    
+    
+    
+    }
+}
---
+++ TriângulosCsAP/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace TriângulosCsAP
+{
+    internal class Program
+    {
+       
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculo de Triângulos\n\n1.Calcular um Triângulo\n2.Sair da Aplicação\n");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Methods.InputRegister();
+            }
+            else if (answer == "2")
+            {
+                Console.WriteLine("Finalizando o Programa");
+                Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Erro, Escolha uma Opção Válida");
+                Console.ReadLine();
+                MainMenu();
+            }
+        }
+
+
+        public static void Enquire()
+        {
+            Console.WriteLine("\nPrograma Finalizado\n\n1.Calcular outro Triângulo\n2.Voltar ao Menu");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Methods.InputRegister();
+            }
+            else if (answer == "2")
+            {
+                Console.WriteLine("FInalizando o Programa....");
+                MainMenu();
+            }
+            else
+            {
+                Console.WriteLine("Erro, Digite uma Resposta Válida\n");
+                Console.ReadLine();
+                Enquire();
+            }
+
+        
+        
+        }
+
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+
+
+
+    }
+    
+    }
+    
+
